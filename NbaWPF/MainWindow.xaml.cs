aea87ff8068fd7a553d3f08933c809e13ebed6be
Ibrahim Yücel <yuecel.i04@htlwienwest.at>
2022-06-08 23:14:46 +0200

    wpf anfang

---
+++ NbaWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using NbaWPF.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NbaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void pageload(Page pageToLoad)
+        {
+            MainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            MainFrame.Content = pageToLoad;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPage loginPage = new LoginPage();
+            pageload(loginPage);
+        }
+
+        private void Startshopping_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateBill_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
